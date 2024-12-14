375aaac072f3ce76f753acd2f1c31cc47a4dec91
Raembus07 <josia.schweizer@gmail.com>
2024-12-14 14:00:09 +0100

    added tests and md file

--- Project346LambdaFunction/src/Project346LambdaFunction/obj/Debug/net8.0/Project346LambdaFunction.AssemblyInfo.cs
+++ Project346LambdaFunction/src/Project346LambdaFunction/obj/Debug/net8.0/Project346LambdaFunction.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project346LambdaFunction")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+691ae76f28fce60338e1d3b6009720bf553aeddf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbff444c9812af8bd32cb55e93d919c8fa6f51c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project346LambdaFunction")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project346LambdaFunction")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
